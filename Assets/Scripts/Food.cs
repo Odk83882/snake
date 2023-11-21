3251a4f14fb9c6f50d7791d2f758007a3399b6b4
LAB1-Student03 <lab1-student03@onlineszkolafilmowa.com.pl>
2023-11-21 11:47:48 +0100

    snak

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,12 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Food : MonoBehaviour
 {
 
+
     [SerializeField]
     private ScoreManager scoreManager;
     public BoxCollider2D gridArea;
     
+    [SerializeField] private ParticleSystem boom;
 
     private void Start()
     {
@@ -22,10 +26,20 @@ public class Food : MonoBehaviour
 
         this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
     }
+    IEnumerator courutineA()
+    {
+        yield return new WaitForSeconds(0.1f);
+        yield return StartCoroutine(corutineB());
+    }
 
+    IEnumerator corutineB()
+    {
+        yield return new WaitForSeconds(0.1f);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+       
+        boom.Play();
         ScoreManager.instance.AddPoint();
         if (other.tag == "Player") {
               RandomizePosition();
